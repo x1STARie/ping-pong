eff3a84a0e5bdd1ee7ae37b9805e36d7db56d569
may <83951515+x1STARie@users.noreply.github.com>
2023-04-26 20:21:19 +0500

    Update 2.0
    Project is ready

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ font2 = font.Font(None, 36)
 bg_color = (0, 170, 255)
 img_player = "racket.png"
 img_ball = 'tenis_ball.png'
-move_h = True
-move_h1 = False
+top = False
+down = False
+left = False
+right = True
+score1 = 0 
+score2 = 0
 
 
 #класс-родитель для других спрайтов
@@ -38,25 +42,104 @@ class Player(GameSprite):
     #метод для управления спрайтом стрелками клавиатуры
     def update_r(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 10:
+        if keys[K_UP] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < win_height - 153:
+        if keys[K_DOWN] and self.rect.y < win_height - 126:
             self.rect.y += self.speed
+        return self.rect.y
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_w] and self.rect.y > 10:
+        if keys[K_w] and self.rect.y > 0:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < win_height - 153:
+        if keys[K_s] and self.rect.y < win_height - 126:
             self.rect.y += self.speed
+        return self.rect.y
 
 class Ball(GameSprite):
     def update(self):
-        if self.rect.y > 50:
+        global right
+        global left
+        global top
+        global down
+        global score1
+        global score2
+
+        if sprite.collide_rect(ten_ball, Player1) and Player1.update_l() <= self.rect.y + 25 and self.rect.y + 25 < Player1.update_l() + 45:
+            top = False
+            left = False
+            down = True
+            right = True
+
+        if sprite.collide_rect(ten_ball, Player1) and Player1.update_l() + 45 <= self.rect.y + 25 and self.rect.y + 25 < Player1.update_l() + 91:
+            top = False
+            left = False
+            down = False
+            right = True
+
+        if sprite.collide_rect(ten_ball, Player1) and Player1.update_l() + 91 <= self.rect.y + 25 and self.rect.y + 25 <= Player1.update_l() + 136:
+            top = True
+            left = False
+            down = False
+            right = True
+
+        if sprite.collide_rect(ten_ball, Player2) and Player2.update_r() <= self.rect.y + 25 and self.rect.y + 25 < Player2.update_r() + 45:
+            top = False
+            left = True
+            down = True
+            right = False
+
+        if sprite.collide_rect(ten_ball, Player2) and Player2.update_r() + 45 <= self.rect.y + 25 and self.rect.y + 25 < Player2.update_r() + 91:
+            top = False
+            left = True
+            down = False
+            right = False
+
+        if sprite.collide_rect(ten_ball, Player2) and Player2.update_r() + 91 <= self.rect.y + 25 and self.rect.y + 25 <= Player2.update_r() + 136:
+            top = True
+            left = True
+            down = False
+            right = False
+
+        elif self.rect.y < 0:
+            top = True
+            down = False
+        elif self.rect.y > 450:
+            top = False
+            down = True
+
+        if right == True and top == False and down == False and left == False:
+            self.rect.x += self.speed
+        if right == False and top == False and down == False and left == True:
+            self.rect.x -= self.speed
+        if right == True and top == True and down == False and left == False:
             self.rect.x += self.speed
             self.rect.y += self.speed
-        if self.rect.y < 600:
+        if right == True and top == False and down == True and left == False:
+            self.rect.x += self.speed
+            self.rect.y -= self.speed
+        if right == False and top == True and down == False and left == True:
+            self.rect.x -= self.speed
+            self.rect.y += self.speed
+        if right == False and top == False and down == True and left == True:
             self.rect.x -= self.speed
             self.rect.y -= self.speed
+
+        if self.rect.x < 0:
+            score2 += 1
+            self.rect.x = 315
+            self.rect.y = 225
+            down = False
+            right = False
+            top = False
+            left = True
+        if self.rect.x > 700:
+            score1 += 1
+            self.rect.x = 315
+            self.rect.y = 225
+            down = False
+            right = True
+            top = False
+            left = False
 
 win_width = 700
 win_height = 500
@@ -64,8 +147,8 @@ display.set_caption("Ping-Pong")
 window = display.set_mode((win_width, win_height))
 window.fill(bg_color)
 
-Player1 = Player(img_player, 5, win_height / 2, 39, 136, 13)
-Player2 = Player(img_player, 700 - 39 - 5, win_height / 2, 39, 136, 13)
+Player1 = Player(img_player, 5, 250, 39, 136, 13)
+Player2 = Player(img_player, 656, 250, 39, 136, 13)
 ten_ball = Ball(img_ball, win_width / 2 - 35, win_height / 2 - 25, 50, 50, 10)
 
 finish = False
@@ -78,13 +161,29 @@ while run:
     if not finish:
         window.fill(bg_color)
 
+        text = font2.render(str(score1) + " - " + str(score2), 1, (255, 255, 255))
+        window.blit(text, (325, 20))
+
+        ten_ball.update()
         Player1.update_l()
         Player2.update_r()
-        ten_ball.update()
-
+        
         Player1.reset()
         Player2.reset()
+
         ten_ball.reset()
+
+        if score1 >= 15:
+            score1 = 15
+            finish = True
+            win1 = font2.render("Игрок 1 выиграл!", 1, (255, 255, 255))
+            window.blit(win1, (250, 225))
+        if score2 >= 15:
+            score2 = 15
+            finish = True
+            win2 = font2.render("Игрок 2 выиграл!", 1, (255, 255, 255))
+            window.blit(win2, (250, 225))
+        
         display.update()
     #цикл срабатывает каждую 0.05 секунд
     time.delay(50)
